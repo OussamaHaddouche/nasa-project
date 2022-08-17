03108aa2314b22e5bcaf569764aeaa06e97f6153
Oussama Haddouche <oussama1haddouche2@gmail.com>
2022-08-17 19:18:00 +0100

    add front end client

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import {
+  Arwes,
+  createSounds,
+  createTheme,
+  SoundsProvider,
+  ThemeProvider,
+} from "arwes";
+
+import AppLayout from "./pages/AppLayout";
+
+import { resources, sounds, theme } from "./settings";
+
+const App = () => {
+  return (
+    <ThemeProvider theme={createTheme(theme)}>
+      <SoundsProvider sounds={createSounds(sounds)}>
+        <Arwes
+          animate
+          background={resources.background.large}
+          pattern={resources.pattern}
+        >
+          {(anim) => (
+            <Router>
+              <AppLayout show={anim.entered} />
+            </Router>
+          )}
+        </Arwes>
+      </SoundsProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
