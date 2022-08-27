8d5b58699d98bca042710e590b2ff7725e6ed43e
Oussama Haddouche <oussama1haddouche2@gmail.com>
2022-08-27 02:42:13 +0100

    [SERVER]: serve static front files

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const planetsRouter = require("./routes/planets/planets.router");
 const launchesRouter = require("./routes/launches/launches.router");
@@ -11,7 +12,12 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/planets", planetsRouter);
 app.use("/launches", launchesRouter);
+
+app.get("*/", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
